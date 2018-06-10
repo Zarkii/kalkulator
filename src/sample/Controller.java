fd63106897c6eeda23ac3af0589835483e36443d
Zarkii <35203783+Zarkii@users.noreply.github.com>
2018-06-10 13:14:02 +0200

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,47 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+public class Controller {
+    @FXML
+    public final EventHandler<ActionEvent> jaf() {
+        System.out.println("adwad");
+        return null;
+    }
+    public void proba(){
+        button1.setText("jjjj");
+    }
+    @FXML
+    Button button1 = new Button();
+    /*@FXML
+    Button button2 = new Button();*/
+/*
+    @FXML
+    Button button3 = new Button();
+    @FXML
+    Button button4 = new Button();
+    @FXML
+    Button button5 = new Button();
+    @FXML
+    Button button6= new Button();
+    @FXML
+    Button button7= new Button();
+    @FXML
+    Button button8 = new Button();
+    @FXML
+    Button button9 = new Button();
+    @FXML
+    Button buttonPlus = new Button();
+    @FXML
+    Button buttonEqual = new Button();
+*/
+
+
+    }
+
