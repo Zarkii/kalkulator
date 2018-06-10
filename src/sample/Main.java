a3ce6b7c3b976cbf321bc73e3cc4a92171ae5e20
Zarkii <35203783+Zarkii@users.noreply.github.com>
2018-06-10 13:46:05 +0200

    wiekszy napis

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -22,7 +22,7 @@ public class Main extends Application {
     }
 
 public void blabla(){
-    System.out.println("bla bla bla");
+    System.out.println("bla bla blavghgcvgcv ");
 }
     public static void main(String[] args) {
         launch(args);
