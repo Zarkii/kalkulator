95580b69effbc644b37f52a860811d531209463b
Zarkii <35203783+Zarkii@users.noreply.github.com>
2018-06-10 13:51:45 +0200

    lepsze bbla

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -22,7 +22,8 @@ public class Main extends Application {
     }
 
 public void blabla(){
-    System.out.println("bla bla blavghgcvgcv ");
+    System.out.println("bla bbla blavghgcvgcv ");
+
 
 }
     public static void main(String[] args) {
